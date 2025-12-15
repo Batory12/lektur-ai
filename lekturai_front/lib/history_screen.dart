85aa44a1da77e9de55a33e45401bbb8e56d5cdd9
Bartysek <bartosz.mlodzinski04@gmail.com>
2025-12-15 20:55:31 +0100

    the history should work for matura questions (they aren't saved, it seems)

--- lekturai_front/lib/history_screen.dart
+++ lekturai_front/lib/history_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lekturai_front/api/history.dart';
+import 'package:lekturai_front/services/profile_service.dart';
 import 'package:lekturai_front/widgets/common_scaffold.dart';
 import 'package:lekturai_front/widgets/question_answer_container.dart';
 
@@ -15,60 +17,53 @@ class HistoryScreenState extends State<HistoryScreen> {
   List<Widget> loadedItems = <Widget>[];
   final ScrollController scroller = ScrollController();
   double screenHeight = 0.0;
+  int last_loaded = 0;
+  HistoryApi api = HistoryApi();
+  ProfileService profile = ProfileService();
+  String? uid;
 
   void _onScroll() {
     if (scroller.position.pixels >=
         scroller.position.maxScrollExtent - screenHeight * 2) {
-      loadMoreItems(5);
+      loadMoreItems(last_loaded + 1, last_loaded + 5);
+      last_loaded += 5;
     }
   }
 
-  Future<void> loadMoreItems(int nToLoad) async {
-    //TODO: actually load the items from backend
-    List<Widget> loaded = [
-      QuestionAnswerContainer(
-        questionTitle: "Gibberish question",
-        questionText: "lkshdgdisuabfisadbkjfbkj",
-        answerText: "This is gibberish!",
-        evaluationTitle: "Correct!",
-        evaluationText:
-            "This absolutely is gibberish. It's loading mocked data until I implement actually loading from backend",
-        questionInitiallyLoading: false,
-        evalInitiallyLoading: false,
-        isMatura: false,
-      ),
-      QuestionAnswerContainer(
-        questionTitle: "Gibberish question2",
-        questionText: "lkshdgdisuabfisadbkjfbkj",
-        answerText: "This is gibberish!",
-        evaluationTitle: "Correct!",
-        evaluationText:
-            "This absolutely is gibberish. It's loading mocked data until I implement actually loading from backend",
-        questionInitiallyLoading: false,
-        evalInitiallyLoading: false,
-        isMatura: false,
-      ),
-      QuestionAnswerContainer(
-        questionTitle: "Gibberish question3",
-        questionText: "lkshdgdisuabfisadbkjfbkj",
-        answerText: "This is gibberish!",
-        evaluationTitle: "Correct!",
-        evaluationText:
-            "This absolutely is gibberish. It's loading mocked data until I implement actually loading from backend",
-        questionInitiallyLoading: false,
-        evalInitiallyLoading: false,
-        isMatura: false,
-      ),
-    ];
+  Future<void> loadMoreItems(int from, int to) async {
+    List<HistoryQuestion> questions = await api.getMaturaHistory(
+      from: from,
+      to: to,
+      uid: uid!,
+    );
+    List<QuestionAnswerContainer> loaded = questions
+        .map(
+          (question) => QuestionAnswerContainer(
+            isMatura: true,
+            questionText: question.question,
+            answerText: question.answer,
+            evaluationText: question.eval,
+            evaluationTitle: "${question.points}",
+          ),
+        )
+        .toList();
     setState(() {
       loadedItems.addAll(loaded);
+      if (loaded.length < to - from) {
+        loadedItems.add(Text("No more items to load."));
+      }
     });
   }
 
   @override
   void initState() {
     scroller.addListener(_onScroll);
-    loadMoreItems(5);
+    uid = profile.currentUser?.uid;
+    if (uid == null) {
+      throw Exception("Can't find current user");
+    }
+    loadMoreItems(0, 5);
+    last_loaded = 5;
     super.initState();
   }
 
@@ -77,7 +72,11 @@ class HistoryScreenState extends State<HistoryScreen> {
     screenHeight = MediaQuery.of(context).size.height;
     return CommonScaffold(
       title: 'History',
-      body: ListView(controller: scroller, children: loadedItems),
+      body: ListView(
+        controller: scroller,
+        children: loadedItems,
+        key: ValueKey(loadedItems.length),
+      ),
       showDrawer: true,
     );
   }
