35a5c11cb660a2702ca69511facd265e2d3885a1
Joel Kojma <279745@student.pwr.edu.pl>
2025-12-16 14:00:34 +0100

    fixed getting env port

--- lekturai_back/main.py
+++ lekturai_back/main.py
@@ -8,7 +8,7 @@ load_dotenv()
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
-        host=os.environ.get("LEKTURAI_URL"),
-        port=os.environ.get("LEKTURAI_PORT"),
+        host=os.getenv("LEKTURAI_URL", "0.0.0.0"),
+        port=int(os.getenv("LEKTURAI_PORT", "8000")),
         reload=True,
     )
