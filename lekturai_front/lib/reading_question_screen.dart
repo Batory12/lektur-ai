2a14fa71f5f0dd1156007f7d3383639f21a5a6d9
Bartysek <bartosz.mlodzinski04@gmail.com>
2025-12-17 21:12:42 +0100

    temp commit

--- lekturai_front/lib/reading_question_screen.dart
+++ lekturai_front/lib/reading_question_screen.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:lekturai_front/widgets/common_scaffold.dart';
 import 'package:lekturai_front/widgets/question_answer_container.dart';
 
-class ReadingQuestionsScreen extends StatelessWidget {
+class ReadingQuestionsScreen extends StatefulWidget {
   final String readingName;
   final int? toChapter;
+
   const ReadingQuestionsScreen({
     super.key,
     required this.readingName,
     this.toChapter,
   });
 
+  @override
+  State<ReadingQuestionsScreen> createState() => _ReadingQuestionsScreenState();
+}
+
+class _ReadingQuestionsScreenState extends State<ReadingQuestionsScreen> {
   @override
   Widget build(BuildContext context) {
     return CommonScaffold(
@@ -19,8 +25,8 @@ class ReadingQuestionsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: QuestionAnswerContainer(
           isMatura: false,
-          readingName: readingName,
-          toChapter: toChapter,
+          readingName: widget.readingName,
+          toChapter: widget.toChapter,
         ),
       ),
     );
